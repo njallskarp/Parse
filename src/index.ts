72e6eebee3eb1fc60d152085e0e8a654d22827a9
njall skarphedinsson <njall16@ru.is>
2021-08-16 13:08:12 -0400

    added syntax error for incorrect indents

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,34 @@
 import * as Tokens from "./Tokens";
 import * as Errors from "./Errors";
+import fs from "fs";
 
 const DEFAULT_TAB_SIZE = 2;
 
-const countLeadingSpace = (txt: string) => {
+const countLeadingSpace = (raw: string) => {
 	let count = 0;
-	while (txt.length > count - 1 && txt.charAt(count) == Tokens.WhiteSpace.DOUBLE_SPACE)
-		count++;
+	while (raw.length > count - 1 && raw.charAt(count) === Tokens.WhiteSpace.SPACE)
+		count += 1;
 	return count;
 };
 
-const preprocess = (raw: string): [number, number, string][] => {
+const preprocess = (raw: string): [number, number, string[]][] => {
 	const lines = raw.split(Tokens.WhiteSpace.NEWLINE);
 	return lines.map((line, i) => {
 		const leadingSpace = countLeadingSpace(line);
 		const lineNumber = i + 1;
-		const adjustedLeadingSpace = leadingSpace % DEFAULT_TAB_SIZE;
-		if (adjustedLeadingSpace !== 0)
+		const adjustedLeadingSpace = leadingSpace / DEFAULT_TAB_SIZE;
+		const isLeadingSpaceValid = leadingSpace % DEFAULT_TAB_SIZE;
+		if (isLeadingSpaceValid !== 0)
 			throw new Errors.SyntaxError("Incorrect indentation", lineNumber);
-		return [lineNumber, adjustedLeadingSpace, line];
+		return [lineNumber, adjustedLeadingSpace, line.trim().split(" ")];
 	});
 };
 
-console.log(
-	preprocess("  This is a line\n    expect four space\n        this was eight")
-);
+try {
+	const FILE_NAME = "example.parse";
+	const file = fs.readFileSync(FILE_NAME);
+	const contents = file.toString();
+	const lines = preprocess(contents);
+} catch (e) {
+	console.log(`${e.type} at line ${e.lineNumber}... ${e.message}`);
+}
